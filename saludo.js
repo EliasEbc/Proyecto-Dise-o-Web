7d0627e8cd1d2a112752704f10c38764980b109b
Elias Delgado <116825142+EliasEbc@users.noreply.github.com>
2023-04-14 20:42:14 -0600

    Delete saludo.js

--- saludo.js
+++
@@ -1,9 +0,0 @@
-var boton = document.getElementById("miBoton");
-var etiqueta = document.getElementById("numero");
-
-
-boton.addEventListener("click", function() {
-   
-    var numeroActual = parseInt(etiqueta.textContent);
-    etiqueta.textContent = (numeroActual + 1).toString();
-});
